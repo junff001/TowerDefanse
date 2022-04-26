4d150aa98a32703dc2206717d84debbd6f02da8c
junff001 <71419212+junff001@users.noreply.github.com>
2022-04-26 12:12:15 +0900

    타워 설치

--- TowerDefense/Assets/01.Scripts/Manager/BuildManager.cs
+++ TowerDefense/Assets/01.Scripts/Manager/BuildManager.cs
@@ -7,9 +7,47 @@ public class BuildManager : Singleton<BuildManager>
 {
     public GameObject towerPrefab = null;               // 타워 프리팹
 
-    // 타워를 스폰하는 함수
-    public void SpawnTower(Transform tileTransform)
+    private Vector2 mousePosition = Vector2.zero;
+    private Camera mainCam = null;
+    private Ray ray = default;
+    private RaycastHit hit = default;
+    
+
+    void Awake()
     {
-        Instantiate(towerPrefab, tileTransform);
+        mainCam = Camera.main;
+    }
+
+    // 타워를 스폰하는 함수
+    void SpawnTower(Transform tileTransform)
+    {
+        // Tower 가 SpawnTile 자식으로 들어감
+        if (tileTransform.childCount < 1)
+        {
+            Instantiate(towerPrefab, tileTransform);
+        }
+    }
+
+    // 스폰 마우스 포지션을 담는 함수
+    public void SpawnMousePosition(InputAction.CallbackContext context)
+    {
+        mousePosition = context.ReadValue<Vector2>();
+    }
+
+    // 스폰 가능한 타일인지 Raycast 로 체크하는 함수
+    public void SpawnableTileRaycast(InputAction.CallbackContext context)
+    {
+        if (context.ReadValueAsButton())
+        {
+            ray = mainCam.ScreenPointToRay(mousePosition);
+
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity))
+            {
+                if (hit.transform.CompareTag("SpawnTile"))
+                {
+                    SpawnTower(hit.transform);
+                }
+            }
+        }
     }
 }
