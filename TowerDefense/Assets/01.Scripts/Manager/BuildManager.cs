849010849edb75c73bfd1be9fef715c4e392f40e
junff001 <71419212+junff001@users.noreply.github.com>
2022-05-08 16:55:58 +0900

    Update BuildManager.cs

--- TowerDefense/Assets/01.Scripts/Manager/BuildManager.cs
+++ TowerDefense/Assets/01.Scripts/Manager/BuildManager.cs
@@ -2,97 +2,104 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.InputSystem;
+using UnityEngine.Tilemaps;
 
 public class BuildManager : Singleton<BuildManager>
 {
-    public GameObject towerPrefab = null;               // 타워 프리팹
+    [SerializeField] private Tilemap tilemap = null;               // 타일맵
+    [SerializeField] private Tilemap towerTileamp = null;          // 타워 타일맵
+    [SerializeField] private Sprite roadSpite = null;              // 길 스프라이트
+    [SerializeField] private Sprite hillSprite = null;             // 언덕 스프라이트
+    [SerializeField] private Sprite towerSprite = null;            // 타워 스프라이트
+    [SerializeField] private LayerMask towerMask = default;
 
-    private RaycastHit hit = default;
-    private bool isPressLeftClick = false;
-    private bool isInteractionTerm = false;
-    private SpriteRenderer currnetTileSprite = null;    // 컬러를 활성화할 현재 타일의 스프라이트
-    private Tower currentTower = null;                  // 공격 범위릃 활성화할 현재 타워
-    private float termTime = 0.1f;
-    private float currentTermTime = 0f;
+    public GameObject towerPrefab = null;                          // 타워 프리팹
+
+    private Tile currentTile = null;                               // 마우스에 위치한 현재 타일
+    private Vector3Int tilePos = Vector3Int.zero;                  // 타일 위치
+    private Vector3Int beforeTilePos = Vector3Int.zero;            // 이전 타일 위치
+    private List<GameObject> towerList = new List<GameObject>();
 
     void Update()
     {
-        InteractionByTile();
+        TileInMousePosition();
+        TileChecking();
     }
 
     // 타워를 스폰하는 함수
-    void SpawnTower(Transform tileTransform)
+    void SpawnTower(Vector3Int tilePos)
     {
         // Tower 가 SpawnTile 자식으로 들어감
         // 중복 생성 방지
         if (GoldManager.Instance.GoldMinus(100))
         {
-            if (tileTransform.childCount < 1)
+            if (towerList.Count == 0)
             {
-                Instantiate(towerPrefab, tileTransform);
+                towerList.Add(Instantiate(towerPrefab, tilemap.GetCellCenterWorld(tilePos), Quaternion.identity));
+            }
+            else
+            {
+                for (int i = 0; i < towerList.Count; i++)
+                {
+                    if (towerList[i].transform.position == tilemap.GetCellCenterWorld(tilePos))
+                    {
+                        return;
+                    }
+                }
+
+                towerList.Add(Instantiate(towerPrefab, tilemap.GetCellCenterWorld(tilePos), Quaternion.identity));
             }
         }
     }
 
-    // Mouse.RaycastHit() 으로 타일에 따라 상호작용하는 함수
-    void InteractionByTile()
+    // 마우스 위치에 있는 타일
+    void TileInMousePosition()
     {
-        hit = Mouse.RaycastHit();
-        isPressLeftClick = Mouse.isPressLeftClick;
+        Vector2 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);     // 마우스 월드 위치 
+        tilePos = tilemap.WorldToCell(pos);                                    // 마우스 위치에 위치한 타일을 Vector3Int 로 변환
+        currentTile = tilemap.GetTile<Tile>(tilePos);
+    }
 
-        if (hit.transform != null)
+    // 현재 타일이 뭔지 확인하는 함수
+    void TileChecking()
+    {
+        if (currentTile != null)
         {
-            Debug.Log(hit.transform.tag);
-
-            switch (hit.transform.tag)
+            if (currentTile.sprite == roadSpite)
             {
-                case "SpawnTile":
-                {
-                    SpawnTileColorActive(hit.transform.GetComponent<SpriteRenderer>());
-
-                    if (isPressLeftClick && !Mouse.isInteractionTerm)
-                    {                       
-                        SpawnTower(hit.transform);
-                        Mouse.MouseInteractionTerm(0.1f);
-                    }
-                }
-                break; 
-                case "RoadTile":
-                {
-                    if (currnetTileSprite != null)
-                    {
-                        currnetTileSprite.color = Color.white;
-                    }
-                }
-                break;
-                case "Tower":
-                {
-                    if (isPressLeftClick && !Mouse.isInteractionTerm)
-                    {
-                        hit.transform.GetComponent<Tower>().TowerUpgrade();
-                        Mouse.MouseInteractionTerm(0.1f);
-                        Debug.Log(Mouse.isInteractionTerm);
-                    }
-                    // 업그레이드 함수
-                    // 공격 범위 표시 함수
-                }
-                break;
-                default:
-                    break;
+                tilemap.SetColor(beforeTilePos, Color.white);
             }
-        }
-    } 
+            else if (currentTile.sprite == hillSprite)
+            {
+                SpawnTileColorActive();
+
+                if (Input.GetMouseButtonDown(0))
+                {
+                    for (int i = 0; i < towerList.Count; i++)
+                    {
+                        if (towerList[i].transform.position == tilemap.GetCellCenterWorld(tilePos))
+                        {
+                            towerList[i].GetComponent<Tower>().TowerUpgrade();
+                        }
+                    }
+
+                    SpawnTower(tilePos);
+                }
+            }
+        } 
+    }
 
     // 스폰 타일 컬러 활성화하는 함수
-    void SpawnTileColorActive(SpriteRenderer tileSprite)
+    void SpawnTileColorActive()
     {
-        if (currnetTileSprite != null)
+        if (beforeTilePos != tilePos)
         {
-            currnetTileSprite.color = Color.white;
+            tilemap.SetColor(beforeTilePos, Color.white);
         }
 
-        currnetTileSprite = tileSprite;
-        currnetTileSprite.color = Color.red;
+        tilemap.SetTileFlags(tilePos, TileFlags.None);   // 잠금해제
+        tilemap.SetColor(tilePos, Color.red);
+
+        beforeTilePos = tilePos;
     }
 }
