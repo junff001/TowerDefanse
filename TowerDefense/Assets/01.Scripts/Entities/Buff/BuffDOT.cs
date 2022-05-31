07d007396db62d0125ed1c18755657f95e3b0374
Double_U_ <junhyup511@naver.com>
2022-05-31 14:41:36 +0900

    도트 버프 완성

---
+++ TowerDefense/Assets/01.Scripts/Entities/Buff/BuffDOT.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffDOT : BuffBase
+{
+    EnemyBase enemy;
+    private float damageTime;
+    private static readonly float dotTime = 1;
+
+    public BuffDOT(GameObject _affecter, float _duration, float _amplification) : base(_affecter, _duration, _amplification)
+    {
+
+    }
+
+    public override void Init()
+    {
+        enemy = affecter.GetComponent<EnemyBase>();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+        damageTime += Time.deltaTime;
+
+        if(damageTime >= dotTime)
+        {
+            damageTime -= dotTime;
+            HealthSystem health = enemy.GetComponent<HealthSystem>();
+            health.TakeDamage(amplification);
+        }
+    }
+
+    public override void Destroy()
+    {
+        base.Destroy();
+    }
+}
