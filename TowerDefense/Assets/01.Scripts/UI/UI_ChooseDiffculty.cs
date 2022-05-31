7918bf1c060bde9cf436692f39b5d3e91e3c7b38
KimTaeHyen <62549012+teahyen@users.noreply.github.com>
2022-05-31 19:26:52 +0900

    끝
    로비 씬 끝 데이터 이동까지

--- TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
+++ TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using DG.Tweening;
+using UnityEngine.SceneManagement;
 
 public class UI_ChooseDiffculty : MonoBehaviour
 {
     public Button exitBtn;
     public GameObject difficultyPanel;
+
+    public Button eseyBtn;
+    public Button nomalBtn;
+    public Button hardBtn;
 
     void Start()
     {
@@ -15,10 +20,24 @@ public class UI_ChooseDiffculty : MonoBehaviour
         {
             difficultyPanel.transform.DOScale(0, 0.3f);
         });
+
+        eseyBtn.onClick.AddListener(() =>
+        {
+            GoInGame(1);
+        });
+        nomalBtn.onClick.AddListener(() =>
+        {
+            GoInGame(2);
+        });
+        hardBtn.onClick.AddListener(() =>
+        {
+            GoInGame(3);
+        });
     }
 
-    void Update()
+    public void GoInGame(int Level)
     {
-        
+        GameManager.Instance.stageLevel = 3;
+        SceneManager.LoadScene(1);
     }
 }
