41f3a8ee555d9b2a445f34373e3c3ce964a70e7d
juhyeong0823 <juhyeong0823@gmail.com>
2022-06-09 20:05:20 +0900

    보우 테스트하셈

--- TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
+++ TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
@@ -10,34 +10,17 @@ public class UI_ChooseDiffculty : MonoBehaviour
     public Button exitBtn;
     public GameObject difficultyPanel;
 
-    public Button eseyBtn;
-    public Button nomalBtn;
-    public Button hardBtn;
-
     void Start()
     {
         exitBtn.onClick.AddListener(() =>
         {
             difficultyPanel.transform.DOScale(0, 0.3f);
         });
-
-        eseyBtn.onClick.AddListener(() =>
-        {
-            GoInGame(1);
-        });
-        nomalBtn.onClick.AddListener(() =>
-        {
-            GoInGame(2);
-        });
-        hardBtn.onClick.AddListener(() =>
-        {
-            GoInGame(3);
-        });
     }
 
-    public void GoInGame(int Level)
+    public void GoInGame(int Level, string sceneName)
     {
         GameManager.stageLevel = 3;
-        Managers.LoadScene.LoadScene("JuhyeongScene");
+        Managers.LoadScene.LoadScene(sceneName);
     }
 }
