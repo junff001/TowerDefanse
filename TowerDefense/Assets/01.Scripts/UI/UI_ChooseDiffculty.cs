7bb80fdd59b798806fedcc7a0d00af22eb0f8132
juhyeong0823 <juhyeong0823@gmail.com>
2022-06-09 20:06:44 +0900

    타이틀씬 임시로 바꿔놓음

--- TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
+++ TowerDefense/Assets/01.Scripts/UI/UI_ChooseDiffculty.cs
@@ -10,17 +10,34 @@ public class UI_ChooseDiffculty : MonoBehaviour
     public Button exitBtn;
     public GameObject difficultyPanel;
 
+    public Button eseyBtn;
+    public Button nomalBtn;
+    public Button hardBtn;
+
     void Start()
     {
         exitBtn.onClick.AddListener(() =>
         {
             difficultyPanel.transform.DOScale(0, 0.3f);
         });
+
+        eseyBtn.onClick.AddListener(() =>
+        {
+            GoInGame(1);
+        });
+        nomalBtn.onClick.AddListener(() =>
+        {
+            GoInGame(2);
+        });
+        hardBtn.onClick.AddListener(() =>
+        {
+            GoInGame(3);
+        });
     }
 
-    public void GoInGame(int Level, string sceneName)
+    public void GoInGame(int Level)
     {
         GameManager.stageLevel = 3;
-        Managers.LoadScene.LoadScene(sceneName);
+        Managers.LoadScene.LoadScene("JuhyeongScene");
     }
 }
