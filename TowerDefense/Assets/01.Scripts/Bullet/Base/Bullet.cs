8f29b67e15ae7fb58cc75297bef0e73af45d8439
juhyeong0823 <juhyeong0823@gmail.com>
2022-06-16 21:55:07 +0900

    불렛쪽 위치 예측 함수 수정중

--- TowerDefense/Assets/01.Scripts/Bullet/Base/Bullet.cs
+++ TowerDefense/Assets/01.Scripts/Bullet/Base/Bullet.cs
@@ -84,66 +84,27 @@ public abstract class Bullet : MonoBehaviour
         gameObject.SetActive(false);
     }
 
-    protected Vector2 GetExpectPos(EnemyBase enemy, float maxTime)
+    protected void GetExpectPos(EnemyBase enemy, float maxTime)
     {
-        //현재 목표 웨이포인트
-        Vector2 destPoint = Managers.Game.wayPoints[enemy.CurrentWayPointIndex].position;
-        //이동하고 있는 방향
-        Vector2 movingDir = (destPoint - (Vector2)enemy.transform.position).normalized;
-        // maxTime이 지난 후, 적의 위치
-        Vector2 enemyPosLaterMaxTime = (Vector2)enemy.transform.position + (movingDir * enemy.enemyData.MoveSpeed * maxTime);
-        // 예상 위치
-        
-        // 목표지점을 넘어선 경우인데, 다음 웨이포인트가 존재한다면.
-        if (CheckPass(destPoint, movingDir, enemyPosLaterMaxTime) && Managers.Game.wayPoints.Count > (enemy.CurrentWayPointIndex + 1))
-        {
-            return GetLerpPoint(enemy, enemyPosLaterMaxTime);
-        }
-        else
-        {
-            return enemyPosLaterMaxTime;
-        }
+        Vector2 curPos = enemy.transform.position;
+        float moveDistWhileMaxTime = enemy.enemyData.MoveSpeed * maxTime;
+
+        int wayPointIdx = GetExpectPos(curPos, moveDistWhileMaxTime, enemy.CurrentWayPointIndex);
     }
 
-    public Vector2 GetLerpPoint(EnemyBase enemy, Vector2 expectPos) // 원래 목적지를 넘어갔을때, 내가 지나간 만큼을 빼주는.. 그런걸 해보자
+    public Vector2 GetExpectPos(Vector2 startPos, float moveDistWhileMaxTime, int index)
     {
-        //지점을 지나면서 인덱스가 1 추가 됐을거임.
-        Vector2 dest = Managers.Game.wayPoints[enemy.CurrentWayPointIndex- 1].position;
-        Vector2 next = Managers.Game.wayPoints[enemy.CurrentWayPointIndex].position;
-        Vector2 moveDir = (next - dest).normalized;
+        Vector2 destPos = Managers.Game.wayPoints[index].position;
 
-        float overDist = Vector2.Distance(dest, expectPos);
-
-        if (moveDir.x != 0)
+        float distToWaypoint = Vector2.Distance(startPos, destPos);
+        if(moveDistWhileMaxTime > distToWaypoint)
         {
-            if (moveDir.x > 0) dest.x += overDist;
-            else dest.x -= overDist;
+            return GetExpectPos(destPos, moveDistWhileMaxTime, ++index);
         }
         else
         {
-            if (moveDir.y > 0) dest.y += overDist;
-            else dest.y -= overDist;
-        }
-
-        return expectPos;
-    }
-
-
-
-    protected bool CheckPass(Vector2 destPoint, Vector2 movingDir, Vector2 enemyPosLaterMaxTime)
-    {
-        if (movingDir.x != 0) // 옆으로 이동중
-            return CheckPassedPoint(movingDir.x, enemyPosLaterMaxTime.x, destPoint.x);
-        else
-            return CheckPassedPoint(movingDir.y, enemyPosLaterMaxTime.y, destPoint.y);
-
-
-        //이동중인 축의 값, 예상 위치 축 값, 목적지 축 값
-        bool CheckPassedPoint(float movingAxisValue, float expect, float dest)
-        {
-            if (movingAxisValue > 0 && false == expect >= dest) return true;
-            else if (movingAxisValue < 0 && false == expect <= dest) return true;
-            return false;
+            Vector2 moveDir = (destPos - startPos).normalized;
+            return startPos += moveDistWhileMaxTime * moveDir;
         }
     }
 
